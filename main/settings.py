4428888297aa9867941660051fedab47de679899
V Shree Harshitha <vsh9945@gmail.com>
2024-08-13 20:32:17 +0530

    requirements updated

--- main/settings.py
+++ main/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alumni',  #database name
+        'USER': 'vsh',   # user name
+        'PASSWORD': 'vsh@9945', 
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
